94c15ad6c61649aae042d006245eaefede711a3b
Marcus Ooi <19mokj97@gmail.com>
2018-09-08 07:03:19 +1000

    Applied basic style to Google Map component

--- src/Homepage.js
+++ src/Homepage.js
@@ -62,7 +62,15 @@ class Homepage extends Component {
                 bootstrapURLKeys={{ key: "AIzaSyCOTCo9ek5D5h2D4IUxnb9ZVkoK6-QP0-g" }}
                 defaultCenter={this.props.center}
                 defaultZoom={this.props.zoom}
-                className="Homepage-googleMap"
+                style={
+                    {
+                    "position": "absolute",
+                    "right": "0",
+                    "top": "0",
+                    "padding": "10px",
+                    "width": "62%",
+                    "height": "100%"}
+                }
             >
             </GoogleMapReact>
             </div>
