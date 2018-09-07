5af0999c782dfab4fc2faaaf86f3da81d2e2e49a
Jonathan Yeung <jonathanyeung1997@gmail.com>
2018-09-08 08:49:58 +1000

    Implemented BrowserRouter

--- src/index.js
+++ src/index.js
@@ -4,5 +4,9 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import {BrowserRouter as Router,
+		Route,Link
+} from 'react-router-dom';
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
