d98458b3025132b1f75144c2057e81ff4115c353
Marcus Ooi <19mokj97@gmail.com>
2018-09-08 06:38:08 +1000

    Checkboxes for data set selection

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Homepage from './Homepage';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div className="App">
+            <Switch>
+              {/* <header className="App-header"> */}
+                {/* <img src={logo} className="App-logo" alt="logo" /> */}
+                {/* <h1 className="App-title">Welcome  to React</h1> */}
+              {/* </header> */}
+              {/* <p className="App-intro">
+                To get started, edit <code>src/App.js</code> and save to reload.
+              </p> */}
+              <Route exact path="/" component={Homepage} />
+            </Switch>
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/Homepage.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import './Homepage.css';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+
+class Homepage extends Component {
+    state = {
+        population: true,
+        crimeRate: false,
+        school: false,
+        housing: false,
+        jobs: false
+    }
+
+    _handleChange = name => event => {
+        this.setState({ [name]: event.target.checked });
+    };
+
+    render() {   
+        const { classes } = this.props;
+        const { population, crimeRate, school, housing, jobs} = this.state;
+        return (
+            <div className="Homepage">
+            {/* <div className={classes.root}> */}
+                <FormControl component="fieldset" className="Homepage-formControl">
+                <FormLabel component="legend">Select what to display</FormLabel>
+                <FormGroup>
+                    <FormControlLabel
+                    control={
+                        <Checkbox checked={population} onChange={this._handleChange('population')} value="population" />
+                    }
+                    label="Population"
+                    />
+                    <FormControlLabel
+                    control={
+                        <Checkbox checked={crimeRate} onChange={this._handleChange('crimeRate')} value="crimeRate" />
+                    }
+                    label="Crime rate"
+                    />
+                    <FormControlLabel
+                    control={
+                        <Checkbox checked={school} onChange={this._handleChange('school')} value="school" />
+                    }
+                    label="Schools"
+                    />
+                </FormGroup>
+            </FormControl>
+            </div>
+        )
+    };
+}
+
+// Homepage.propTypes = {
+//     classes: PropTypes.object.isRequired,
+// };
+
+
+export default Homepage;
